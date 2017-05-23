b670b8fb71970791028ca659e2ac7d52848b1e93
sau00 <sau00@localhost>
2017-05-23 23:07:01 +0300

    Bot Confirmation

---
+++ modules/bot/controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\modules\bot\controllers;
+
+use app\models\Items;
+use \app\models\Users;
+use app\modules\parsers\models\AwgmModel;
+
+/**
+ * Created by PhpStorm.
+ * User: sau00
+ * Date: 4/27/17
+ * Time: 11:01 PM
+ */
+class MainController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        echo 'cd743aa7';
+    }
+}
