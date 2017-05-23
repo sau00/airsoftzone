90a07fcfea579895a6da4ba3cb5ddb3fff6f65fc
sau00 <sau00@localhost>
2017-05-23 03:28:46 +0300

    Модуль бота

---
+++ modules/bot/Module.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sau00
+ * Date: 5/23/17
+ * Time: 3:25 AM
+ */
+namespace app\modules\bot;
+
+class Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+}
