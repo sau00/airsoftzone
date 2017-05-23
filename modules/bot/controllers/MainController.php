096d127047b547c4ca75b276854505bd9dfd4c2c
sau00 <sau00@localhost>
2017-05-23 23:27:10 +0300

    Disable CSRF

--- modules/bot/controllers/MainController.php
+++ modules/bot/controllers/MainController.php
@@ -25,56 +25,53 @@ class MainController extends \yii\web\Controller
 
     public function actionIndex()
     {
-//        $this->enableCsrfValidation = false;
+        if (!isset($_REQUEST)) {
+            return;
+        }
 
-        echo 'cd743aa7';
-//        if (!isset($_REQUEST)) {
-//            return;
-//        }
-//
-////Строка для подтверждения адреса сервера из настроек Callback API
-//        $confirmation_token = 'cd743aa7';
-//
-////Ключ доступа сообщества
-//        $token = 'c0223f775444cf3d58a8a1442ec76a9571c8f58e3e24616d9440f73dc43022bbead9b2e576cb41d09c0a1';
-//
-////Получаем и декодируем уведомление
-//        $data = json_decode(file_get_contents('php://input'));
-//
-////Проверяем, что находится в поле "type"
-//        switch ($data->type) {
-//            //Если это уведомление для подтверждения адреса сервера...
-//            case 'confirmation':
-//                //...отправляем строку для подтверждения адреса
-//                echo $confirmation_token;
-//                break;
-//
-////Если это уведомление о новом сообщении...
-//            case 'message_new':
-//                //...получаем id его автора
-//                $user_id = $data->object->user_id;
-//                //затем с помощью users.get получаем данные об авторе
-//                $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&v=5.0"));
-//
-////и извлекаем из ответа его имя
-//                $user_name = $user_info->response[0]->first_name;
-//
-////С помощью messages.send и токена сообщества отправляем ответное сообщение
-//                $request_params = array(
-//                    'message' => "Hello, {$user_name}!",
-//                    'user_id' => $user_id,
-//                    'access_token' => $token,
-//                    'v' => '5.0'
-//                );
-//
-//                $get_params = http_build_query($request_params);
-//
-//                file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
-//
-////Возвращаем "ok" серверу Callback API
-//                echo('ok');
-//
-//                break;
-//        }
+//Строка для подтверждения адреса сервера из настроек Callback API
+        $confirmation_token = 'cd743aa7';
+
+//Ключ доступа сообщества
+        $token = '630aa6da5928b5da9977d64d44b8040f83e366b3bea1cc03872bfff8432fb2c5114022312da80a6ba41b5';
+
+//Получаем и декодируем уведомление
+        $data = json_decode(file_get_contents('php://input'));
+
+//Проверяем, что находится в поле "type"
+        switch ($data->type) {
+            //Если это уведомление для подтверждения адреса сервера...
+            case 'confirmation':
+                //...отправляем строку для подтверждения адреса
+                echo $confirmation_token;
+                break;
+
+//Если это уведомление о новом сообщении...
+            case 'message_new':
+                //...получаем id его автора
+                $user_id = $data->object->user_id;
+                //затем с помощью users.get получаем данные об авторе
+                $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&v=5.0"));
+
+//и извлекаем из ответа его имя
+                $user_name = $user_info->response[0]->first_name;
+
+//С помощью messages.send и токена сообщества отправляем ответное сообщение
+                $request_params = array(
+                    'message' => "Привет, {$user_name}!",
+                    'user_id' => $user_id,
+                    'access_token' => $token,
+                    'v' => '5.0'
+                );
+
+                $get_params = http_build_query($request_params);
+
+                file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
+
+//Возвращаем "ok" серверу Callback API
+                echo('ok');
+
+                break;
+        }
     }
 }
