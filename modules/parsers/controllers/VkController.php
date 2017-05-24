d6e64ba0339e0f24452c8059404ce4f4a3c8726b
sau00 <sau00@localhost>
2017-05-24 03:50:22 +0300

    Псевдо-чешуйки в поиске

--- modules/parsers/controllers/VkController.php
+++ modules/parsers/controllers/VkController.php
@@ -85,13 +85,37 @@ class VkController extends \yii\web\Controller
                         }
                     }
 
-                    $vk_item->description = trim(strip_tags($vk_item->description));
+                    function rus2translit($string) {
+                        $converter = array(
+                            'а' => 'a',   'б' => 'b',   'в' => 'v',
+                            'г' => 'g',   'д' => 'd',   'е' => 'e',
+                            'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
+                            'и' => 'i',   'й' => 'y',   'к' => 'k',
+                            'л' => 'l',   'м' => 'm',   'н' => 'n',
+                            'о' => 'o',   'п' => 'p',   'р' => 'r',
+                            'с' => 's',   'т' => 't',   'у' => 'u',
+                            'ф' => 'f',   'х' => 'h',   'ц' => 'c',
+                            'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
+                            'ь' => '\'',  'ы' => 'y',   'ъ' => '\'',
+                            'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
+                            ' ' => ' ',
 
-                    $vk_item->description = preg_replace ("/[^a-zA-ZА-Яа-я0-9\s]/","", $vk_item->description);
+                            'А' => 'A',   'Б' => 'B',   'В' => 'V',
+                            'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
+                            'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
+                            'И' => 'I',   'Й' => 'Y',   'К' => 'K',
+                            'Л' => 'L',   'М' => 'M',   'Н' => 'N',
+                            'О' => 'O',   'П' => 'P',   'Р' => 'R',
+                            'С' => 'S',   'Т' => 'T',   'У' => 'U',
+                            'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
+                            'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
+                            'Ь' => '\'',  'Ы' => 'Y',   'Ъ' => '\'',
+                            'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya'
+                        );
+                        return strtr($string, $converter);
+                    }
 
-                    echo $vk_item->description;
-
-                    exit();
+                    $vk_item->description = preg_replace("/[^A-Za-z0-9 ]/", '', rus2translit(trim(strip_tags($vk_item->description))));
 
                     if (!VkItems::findOne(['md5' => $vk_item->md5])) {
                         if (strlen($vk_item->description) > 10) {
