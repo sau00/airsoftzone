e2179c004898aa07db290aec54f0e3b9e6aa4536
sau00 <sau00@localhost>
2017-05-24 03:31:13 +0300

    VkModel

---
+++ models/VkItems.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "vk_items".
+ *
+ * @property integer $id
+ * @property string $photo
+ * @property string $description
+ * @property string $url
+ * @property string $md5
+ * @property integer $author_id
+ * @property integer $group_id
+ */
+class VkItems extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'vk_items';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['photo', 'description', 'url', 'md5', 'author_id', 'group_id'], 'required'],
+            [['photo', 'description', 'url'], 'string'],
+            [['author_id', 'group_id'], 'integer'],
+            [['md5'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'photo' => 'Photo',
+            'description' => 'Description',
+            'url' => 'Url',
+            'md5' => 'Md5',
+            'author_id' => 'Author ID',
+            'group_id' => 'Group ID',
+        ];
+    }
+}
