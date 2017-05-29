059c647f8a3335d267f4ece4ddd47c10bf13e039
sau00 <sau00@localhost>
2017-05-30 02:47:50 +0300

    Обновление бота

--- modules/bot/controllers/MainController.php
+++ modules/bot/controllers/MainController.php
@@ -29,46 +29,55 @@ class MainController extends \yii\web\Controller
             return;
         }
 
-//Строка для подтверждения адреса сервера из настроек Callback API
         $confirmation_token = 'cd743aa7';
 
-//Ключ доступа сообщества
         $token = '630aa6da5928b5da9977d64d44b8040f83e366b3bea1cc03872bfff8432fb2c5114022312da80a6ba41b5';
 
-//Получаем и декодируем уведомление
         $data = json_decode(file_get_contents('php://input'));
 
-//Проверяем, что находится в поле "type"
         switch ($data->type) {
-            //Если это уведомление для подтверждения адреса сервера...
             case 'confirmation':
-                //...отправляем строку для подтверждения адреса
                 echo $confirmation_token;
                 break;
 
-//Если это уведомление о новом сообщении...
             case 'message_new':
-                //...получаем id его автора
                 $user_id = $data->object->user_id;
-                //затем с помощью users.get получаем данные об авторе
-                $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&v=5.0"));
 
-//и извлекаем из ответа его имя
+                $message = $data->object->body;
+
+                $messages = [
+                    '/add' => "Для добавления объявления отправь в ответном сообщении форму и прикрепи фотографию:" . "\r\n" .
+                        "1) Заголовок объявления"  . "\r\n" .
+                        "2) Цена" . "\r\n" .
+                        "3) Город" . "\r\n" .
+                        "4) Пересыл (Да/Нет)" . "\r\n" .
+                        "5) Описание" . "\r\n"
+                ];
+
+                $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&lang=ru&v=5.0"));
+
                 $user_name = $user_info->response[0]->first_name;
 
-//С помощью messages.send и токена сообщества отправляем ответное сообщение
-                $request_params = array(
-                    'message' => "Привет, {$user_name}! Я пока отдыхаю, для поиска объявлений можешь воспользоваться сайтом http://airsoftzone.ru/index.php?r=site/vk",
-                    'user_id' => $user_id,
-                    'access_token' => $token,
-                    'v' => '5.0'
-                );
+                if($message == '/add') {
+                    $request_params = array(
+                        'message' => $messages[$message],
+                        'user_id' => $user_id,
+                        'access_token' => $token,
+                        'v' => '5.0'
+                    );
+                } else {
+                    $request_params = array(
+                        'message' => "Привет, {$user_name}! Я пока отдыхаю, для поиска объявлений можешь воспользоваться сайтом http://airsoftzone.ru/index.php?r=site/vk",
+                        'user_id' => $user_id,
+                        'access_token' => $token,
+                        'v' => '5.0'
+                    );
+                }
 
                 $get_params = http_build_query($request_params);
 
                 file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
 
-//Возвращаем "ok" серверу Callback API
                 echo('ok');
 
                 break;
