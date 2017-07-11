d65de6f437503863230101ed1d5cbe0e955cce5d
sau00 <sau00@localhost>
2017-07-11 22:03:49 +0300

    Google Analytics

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -77,6 +77,16 @@ AppAsset::register($this);
                     "border='0' width='31' height='31'><\/a>")
             </script><!--/LiveInternet-->
         </p>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-80755700-4', 'auto');
+            ga('send', 'pageview');
+
+        </script>
     </div>
 </footer>
 
